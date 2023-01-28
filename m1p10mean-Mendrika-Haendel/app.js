1028f8aee8590a4c2810a00b9d402378d58a4e9d
HaendelR <89445756+HaendelR@users.noreply.github.com>
2023-01-28 14:01:49 +0300

    .env

--- m1p10mean-Mendrika-Haendel/app.js
+++ m1p10mean-Mendrika-Haendel/app.js
@@ -1,4 +1,5 @@
 var createError = require("http-errors");
+require("dotenv").config();
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
@@ -8,8 +9,10 @@ const cors = require("cors");
 
 var mongo = require("mongodb");
 var monk = require("monk");
-// var db = monk("localhost:27017/garage");
-var db = monk("mongodb+srv://basedonnees:projetRojo@cluster0.act2jb1.mongodb.net/garage?retryWrites=true&w=majority");
+//var db = monk("localhost:27017/garage");
+var db = monk(
+  "mongodb+srv://basedonnees:projetRojo@cluster0.act2jb1.mongodb.net/garage?retryWrites=true&w=majority"
+);
 
 // var indexRouter = require('./routes/index');
 var usersRouter = require("./routes/users");
@@ -25,6 +28,7 @@ var invoiceRouter = require("./routes/invoice");
 var sendMailRouter = require("./routes/sendMail");
 
 var app = express();
+app.set("port", process.env.PORT || 3000);
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
